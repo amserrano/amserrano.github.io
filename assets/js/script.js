14958b46e1afd6ee64316b693f7064250c5de0d9
alejandro serrano <alex.serrano13@gmail.com>
2016-11-29 23:17:14 -0800

    doneee
    yesss

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,45 +25,23 @@ $(function() {
      })
 });
 
+ $(document) .ready(function() {
+
+    $("#photos img:gt(0)") .hide();
+
+    setInterval (function(){
+
+        var current = $('#photos img:visible');
+
+        var next = current.next().length ? current.next() : $('#photos img:eq(0)');
+
+        current.fadeOut(1000);
+
+        next.fadeIn(1000);
+
+    }, 3000);
+ });
 
 
-'use strict';
 
-$(function() {
-
-    //settings for slider
-    var width = 520;
-    var animationSpeed = 1000;
-    var pause = 3000;
-    var currentSlide = 1;
-
-    //cache DOM elements
-    var $slider = $('#slider');
-    var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
-
-    var interval;
-
-    function startSlider() {
-        interval = setInterval(function() {
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (++currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                }
-            });
-        }, pause);
-    }
-    function pauseSlider() {
-        clearInterval(interval);
-    }
-
-    $slideContainer
-        .on('mouseenter', pauseSlider)
-        .on('mouseleave', startSlider);
-
-    startSlider();
-
-});
-
-
+    
