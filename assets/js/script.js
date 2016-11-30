e76b897de1365d99ce139b8f7d3e4365af8771fb
alejandro serrano <alex.serrano13@gmail.com>
2016-11-29 19:45:59 -0800

    happy
    yolo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,22 @@ $(function() {
           $(this).addClass("active");
      })
 });
+
+(function() {
+     var pgurl = window.location.href.substr(window.location.href
+.lastIndexOf("/")+1);
+     $("#nav ul li a").each(function(){
+          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+          $(this).addClass("active1");
+     })
+});
+
+(function() {
+     var pgurl = window.location.href.substr(window.location.href
+.lastIndexOf("/")+1);
+     $("#nav ul li a").each(function(){
+          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+          $(this).addClass("active2");
+     })
+});
+
