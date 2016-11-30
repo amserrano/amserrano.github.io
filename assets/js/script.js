bd4292c844ab5cad1ad179a22424ae8c805fcbd3
alejandro serrano <alex.serrano13@gmail.com>
2016-11-29 21:23:29 -0800

    pls
    need this

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(function() {
 .lastIndexOf("/")+1);
      $("#nav ul li a").each(function(){
           if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-          $(this).addClass("active");
+          $(this).addClass("index");
      })
 });
 
@@ -12,7 +12,7 @@ $(function() {
 .lastIndexOf("/")+1);
      $("#nav ul li a").each(function(){
           if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-          $(this).addClass("active1");
+          $(this).addClass("page1");
      })
 });
 
@@ -21,9 +21,49 @@ $(function() {
 .lastIndexOf("/")+1);
      $("#nav ul li a").each(function(){
           if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-          $(this).addClass("active2");
+          $(this).addClass("page2");
      })
 });
 
+
+
+'use strict';
+
+$(function() {
+
+    //settings for slider
+    var width = 520;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+});
 
 
